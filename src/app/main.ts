baab2f43c19799c562bfa4928645983b314d3bd0
Bruno Mello <brunomello.ti@gmail.com>
2026-05-13 11:20:23 -0300

    feat: remove cors

--- src/app/main.ts
+++ src/app/main.ts
@@ -7,6 +7,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  });
+
   app.enableShutdownHooks();
 
   if (server.config.nodeEnv !== 'production') {
