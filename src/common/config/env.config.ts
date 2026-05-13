35e3706a5a96ce3b0bb6dde60ef31f2a9e5b92b9
Bruno Mello <brunomello.ti@gmail.com>
2026-05-13 11:20:57 -0300

    Remove default CORS origin settings
    Removed default CORS origin configuration from env.config.ts.

--- src/common/config/env.config.ts
+++ src/common/config/env.config.ts
@@ -8,10 +8,4 @@ export const server = {
         port: process.env.PORT ? +process.env.PORT : 3000,
         name: process.env.APP_NAME || 'type-race-backend',
         nodeEnv: process.env.NODE_ENV || 'development',
-        corsOrigin:
-            process.env.CORS_ORIGIN?.split(',') ?? [
-                'http://localhost:5173',
-                'http://localhost:3000',
-            ],
-    },
 };
